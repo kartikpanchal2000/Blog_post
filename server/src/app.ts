a9145e8a41f216a5001ebe6ae3bb00244816f0bd
Kartik Panchal <kartikpanchal@gmail.com>
2026-05-01 15:15:25 +0530

    adding lines  of code for production

--- server/src/app.ts
+++ server/src/app.ts
@@ -13,7 +13,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
 	cors({
-		origin: process.env.CLIENT_URL || 'http://localhost:5173',
+		//origin: process.env.CLIENT_URL || 'http://localhost:5173',
+		origin: true,
 		methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 		allowedHeaders: ['Content-Type', 'Authorization'],
 	}),
