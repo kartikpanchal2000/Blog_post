d4c351cc0764fb63fed4f85cc72bae0facdf284d
Kartik Panchal <kartikpanchal@gmail.com>
2026-05-05 17:20:28 +0530

    fix: add real vercel url to cors1

--- server/src/app.ts
+++ server/src/app.ts
@@ -16,7 +16,7 @@ app.use(
 		//origin: process.env.CLIENT_URL || 'http://localhost:5173',
 		origin: [
 			'http://localhost:5173',
-			'https://blog-post-kappa-eight.vercel.app/',
+			'https://blog-post-kappa-eight.vercel.app',
 		],
 		methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 		allowedHeaders: ['Content-Type', 'Authorization'],
